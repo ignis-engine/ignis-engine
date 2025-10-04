9c554bbaa9215f876b610fa588e8cdf1e9d8d87b
Robin 'Ruadeil' Degen <mail@ruadeil.lgbt>
2025-10-04 23:50:19 +0200

    Add render pass methods

--- Ignis/GPU/SdlGpuRenderPass.cs
+++ Ignis/GPU/SdlGpuRenderPass.cs
@@ -22,6 +22,47 @@ internal unsafe class SdlGpuRenderPass : ReferenceCountedBase, IDisposable
         NativeRenderPass = SDL3.SDL_BeginGPURenderPass(commandBuffer.NativeCommandBuffer, &info, 1, null);
     }
 
+    public void BindGraphicsPipeline(SdlGpuGraphicsPipeline pipeline)
+    {
+        SDL3.SDL_BindGPUGraphicsPipeline(NativeRenderPass, pipeline.NativeGraphicsPipeline);
+    }
+
+    public void BindVertexBuffer(SdlGpuBuffer vertexBuffer, uint firstSlot = 0, uint offset = 0)
+    {
+        var region = new SDL_GPUBufferBinding { buffer = vertexBuffer.NativeBuffer, offset = offset };
+        SDL3.SDL_BindGPUVertexBuffers(NativeRenderPass, firstSlot, &region, 1);
+    }
+
+    public void BindIndexBuffer(
+        SdlGpuBuffer indexBuffer,
+        uint offset = 0,
+        SDL_GPUIndexElementSize elementSize = SDL_GPUIndexElementSize.SDL_GPU_INDEXELEMENTSIZE_32BIT
+    )
+    {
+        var region = new SDL_GPUBufferBinding { buffer = indexBuffer.NativeBuffer, offset = offset };
+        SDL3.SDL_BindGPUIndexBuffer(NativeRenderPass, &region, elementSize);
+    }
+
+    public void BindFragmentSamplers(SdlGpuTexture texture, SdlGpuSampler sampler, uint index)
+    {
+        var binding = new SDL_GPUTextureSamplerBinding
+        {
+            texture = texture.NativeTexture,
+            sampler = sampler.NativeSampler,
+        };
+        SDL3.SDL_BindGPUFragmentSamplers(NativeRenderPass, index, &binding, 1);
+    }
+
+    public void DrawPrimitives(uint vertexCount, uint firstVertex)
+    {
+        SDL3.SDL_DrawGPUPrimitives(NativeRenderPass, vertexCount, 1, firstVertex, 0);
+    }
+
+    public void DrawIndexedPrimitives(uint numIndices, uint startIndex, int vertexOffset)
+    {
+        SDL3.SDL_DrawGPUIndexedPrimitives(NativeRenderPass, numIndices, 1, startIndex, vertexOffset, 0);
+    }
+
     public void Dispose()
     {
         SDL3.SDL_EndGPURenderPass(NativeRenderPass);
